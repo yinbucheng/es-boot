231adfc1f06e69511b5c215845fb9094dec545d0
yinchong <yin.chong@intellif.com>
2019-07-24 20:59:43 +0800

    完成测试

---
+++ src/main/java/cn/bucheng/esboot/controller/ESController.java
@@ -0,0 +1,26 @@
+package cn.bucheng.esboot.controller;
+
+import cn.bucheng.esboot.dao.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：yinchong
+ * @create ：2019/7/24 20:58
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@RestController
+@RequestMapping("es")
+public class ESController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @RequestMapping("listAll")
+    public Object listAll() {
+        return bookRepository.findAll();
+    }
+}
