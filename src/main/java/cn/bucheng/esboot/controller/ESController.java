3d6ff1a541e8225f3d7bc17990c402d62b3d503e
yinchong <yin.chong@intellif.com>
2019-07-26 20:03:17 +0800

    --

--- src/main/java/cn/bucheng/esboot/controller/ESController.java
+++ src/main/java/cn/bucheng/esboot/controller/ESController.java
@@ -3,7 +3,12 @@ package cn.bucheng.esboot.controller;
 import cn.bucheng.esboot.dao.BookRepository;
 import cn.bucheng.esboot.model.entity.BookEntity;
 import cn.bucheng.esboot.model.vo.BookVO;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +23,12 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("es")
+@Slf4j
 public class ESController {
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private ElasticsearchTemplate template;
 
     @RequestMapping("listAll")
     public Object listAll() {
@@ -33,13 +41,45 @@ public class ESController {
         return bookRepository.findByNameLike(name);
     }
 
-    @RequestMapping("query")
-    public List<BookEntity> queryLike(String param){
-        return bookRepository.findLikeAll(param);
+    @RequestMapping("queryAnd")
+    public List<BookEntity> queryAndLike(String param) {
+        BoolQueryBuilder builder = QueryBuilders.boolQuery()
+                .must(QueryBuilders.termQuery("name", param))
+                .must(QueryBuilders.termQuery("title", param))
+                .must(QueryBuilders.termQuery("content", param))
+                .must(QueryBuilders.termQuery("writer", param));
+        log.info(builder.toString());
+        SearchQuery query = new NativeSearchQuery(builder);
+        return template.queryForList(query, BookEntity.class);
+    }
+
+
+    /**
+     * termQuery("key", obj) 完全匹配
+     * matchQuery("key", Obj) 单个匹配, field不支持通配符, 前缀具高级特性
+     * fuzzyQuery("user", "kimch")  模糊查询,不能用通配符, 找到相似的
+     * wildcardQuery("user", "ki*hy") 通配符查询, 支持 *
+     *
+     * @param param
+     * @return
+     */
+    @RequestMapping("queryOr")
+    public List<BookEntity> queryOrLike(String param) {
+        QueryBuilder builder = QueryBuilders.fuzzyQuery("content", param);
+//                .should(QueryBuilders.fuzzyQuery("name", param))
+//                .should(QueryBuilders.fuzzyQuery("title", param))
+//                .should(QueryBuilders.wildcardQuery("content", param+"*"))
+//                .should(QueryBuilders.fuzzyQuery("writer", param));
+       BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+       boolQueryBuilder.should(builder);
+        log.info(boolQueryBuilder.toString());
+        SearchQuery query = new NativeSearchQuery(boolQueryBuilder);
+        log.info(query.getQuery().toString());
+        return template.queryForList(query, BookEntity.class);
     }
 
     @RequestMapping("queryContent")
-    public List<BookEntity> queryContentLike(String content){
+    public List<BookEntity> queryContentLike(String content) {
         return bookRepository.findByContentLike(content);
     }
 
