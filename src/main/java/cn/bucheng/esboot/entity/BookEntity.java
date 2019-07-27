66593a5cdfe260ea639c8e67923581cb3cef1eef
yinbucheng <yin.chong@intellif.com>
2019-07-27 11:37:46 +0800

    集成spring-boot-starter-mysql-binlog测试

--- src/main/java/cn/bucheng/esboot/entity/BookEntity.java
+++ src/main/java/cn/bucheng/esboot/entity/BookEntity.java
@@ -1,5 +1,6 @@
 package cn.bucheng.esboot.entity;
 
+import cn.bucheng.mysql.annotation.ColumnName;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
@@ -29,6 +30,7 @@ import java.util.Date;
 @ToString
 @Document(indexName = "book", type = "book", shards = 3, replicas = 1)
 @TableName("ad_book")
+@cn.bucheng.mysql.annotation.TableName(schema = "ad_test", table = "ad_book")
 public class BookEntity implements Serializable {
     @Id
     @Field(type = FieldType.Long)
@@ -41,8 +43,10 @@ public class BookEntity implements Serializable {
     @Field(type = FieldType.Keyword)
     private String writer;
     @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis")
+    @ColumnName(sqlColumn = "create_time")
     private Date createTime;
     @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis")
+    @ColumnName(sqlColumn = "update_time")
     private Date updateTime;
     @Field(type = FieldType.Text)
     private String content;
