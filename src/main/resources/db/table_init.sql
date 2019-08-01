40c9cbf74e1e9d4710739e8bbd06d3faa2abcdb6
yinchong <yin.chong@intellif.com>
2019-08-01 09:39:26 +0800

    ---

--- src/main/resources/db/table_init.sql
+++ src/main/resources/db/table_init.sql
@@ -12,7 +12,7 @@ create table if not exists ad_user(
 id bigint  primary key auto_increment,
 name varchar(50),
 age int,
-gender varchar(20) not null default '男' comment `性别 男 女`,
+gender varchar(20) not null default '男' comment '性别 男 女',
 create_time datetime not null default now(),
 update_time datetime not null default now()
 );
